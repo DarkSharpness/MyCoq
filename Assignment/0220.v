1199ac80bd506539c3709e94ffc05019780e76ce
DarkSharpness_XzY <2040703891@qq.com>
2024-02-20 22:16:30 +0800

    Upd: First day writing coq.

---
+++ Assignment/0220.v
@@ -0,0 +1,17 @@
+Require Import Coq.ZArith.ZArith.
+Require Import Coq.micromega.Psatz.
+Require Import PV.Intro.
+Local Open Scope Z.
+
+Definition opposite_sgn (x y: Z): Prop := x * y < 0.
+
+Fact opposite_sgn_plus_2: forall x,
+  opposite_sgn (x + 2) x ->
+  x = -1.
+Proof. unfold opposite_sgn. nia. Qed.
+
+Fact opposite_sgn_odds: forall x,
+  opposite_sgn (square x) x ->
+  x < 0.
+Proof. unfold opposite_sgn, square. nia. Qed.
+
